59c615b5019fe771b540669f0898e2497ac5c9b5
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-06-14 16:11:11 +0200

    HW 08 - Part1.3

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     siteName: 'NoteHub',
     images: [
       {
-        url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
         width: 1200,
         height: 630,
         alt: 'NoteHub',
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -21,7 +21,7 @@ export const generateMetadata = async ({ params }: Props): Promise<Metadata> => 
       siteName: 'NoteHub',
       images: [
         {
-          url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
           width: 1200,
           height: 630,
           alt: note.title,
